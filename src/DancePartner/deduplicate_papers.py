acf1e7360ff53a41a6fe2ec0240b35e6615d56a6
David-Degnan <ddegnan@uoregon.edu>
2025-06-04 14:00:36 -0700

    Created a generic deduping function

--- src/DancePartner/deduplicate_papers.py
+++ src/DancePartner/deduplicate_papers.py
@@ -1,12 +1,78 @@
 import pandas as pd
 
-def _read_csv_or_txt(file_path):
-    '''A simple function to read either a csv or txt file'''
+def _read_csv_or_txt(file_path: str):
+    '''
+    A simple function to read either a csv or txt file
+    
+    Parameters
+    ---------
+    file_path
+        A string indicating the path to the file
+    
+    Returns
+    -------
+        A pandas DataFrame
+    '''
     file_path = str(file_path)
     if ".csv" in file_path:
         return(pd.read_csv(file_path))
     elif ".txt" in file_path:
         return(pd.read_table(file_path))
+    
+def _text_clean(string: str):
+    '''
+    This function cleans up the text of the input string, removing nonalphanumerics.
+    This is essential to proper collapsing papers based on title. 
+
+    Parameters
+    ---------
+    string
+        The input string to clean
+    
+    Return
+    ------
+        The string with non-alphanumerics removed (with the exception of spaces)
+
+    '''
+    return(''.join(char for char in string.lower() if char.isalnum() or char == " "))
+
+
+def _table_merge(pubmed_info: pd.DataFrame, scopus_info: pd.DataFrame, osti_info: pd.DataFrame):
+    '''
+    Merge DOI or Title tables from doi_pull and title_pull. This function allows for proper merging, for any 
+    combination of PubMed, Scopus, or OSTI information.
+
+    Parameters
+    ----------
+    pubmed_info
+        The output from doi_pull or title_pull from pubmed
+
+    scopus_info
+        The output from doi_pull or title_pull from scopus
+
+    osti_info
+        The output from doi_pull and title_pull from osti
+
+    Returns
+    -------
+        A pandas DataFrame        
+    '''
+
+    # Merge as long as the dataframe is not none
+    summary_tables = [pubmed_info, scopus_info, osti_info]
+    exists = [pubmed_info is not None, scopus_info is not None, osti_info is not None]
+    position = [pos for pos in range(len(exists)) if exists[pos] == True]
+
+    # If length 1, no merging is required. If length 2, merge the two together. If length
+    # 3, merge all three togeter. 
+    if sum(exists) == 1: 
+        merged = summary_tables[position[0]]
+    elif sum(exists) == 2:
+        merged = pd.merge(summary_tables[position[0]], summary_tables[position[1]], how = "outer")
+    else: 
+        merged = pubmed_info.merge(scopus_info, how = "outer").merge(osti_info, how = "outer")
+
+    return(merged)
 
 def deduplicate_papers(pubmed_path: str = None, scopus_path: str = None, osti_path: str = None):
     '''
@@ -28,9 +94,23 @@ def deduplicate_papers(pubmed_path: str = None, scopus_path: str = None, osti_pa
         A table with deduplicated papers
     '''
 
-    
+    # If provided, pull essential information from each text file and clean paper titles.
+    pubmed, scopus, osti = None, None, None
+    if pubmed_path is not None:
+        pubmed = _read_csv_or_txt(pubmed_path)
+        pubmed = pubmed[["PMID", "Title", "DOI"]].rename({"PMID":"pubmed"}, axis = 1).sort_values(by = "DOI")
+        pubmed["Title"] = [_text_clean(x) for x in pubmed["Title"]]
+    if scopus_path is not None:
+        scopus = _read_csv_or_txt(scopus_path)
+        scopus = scopus[["Title", "DOI", "EID"]].rename({"EID":"scopus"}, axis = 1).sort_values(by = "DOI")
+        scopus["Title"] = [_text_clean(x) for x in scopus["Title"]]
+    if osti_path is not None:
+        osti = _read_csv_or_txt(osti_path)
+        osti = osti[["TITLE", "DOI", "OSTI_IDENTIFIER"]].rename({"TITLE":"Title", "OSTI_IDENTIFIER":"osti"}, axis = 1).sort_values(by = "DOI")
+        osti["Title"] = [_text_clean(x) for x in osti["Title"]]
 
-
+    # Merge tables 
+    return _table_merge(pubmed, scopus, osti).reset_index(drop = True)[["Title", "DOI", "pubmed", "scopus", "osti"]]
 
 def litportal_deduplicate_papers(pubmed_path: str = None, scopus_path: str = None, osti_path: str = None):
     '''
@@ -74,12 +154,21 @@ def litportal_deduplicate_papers(pubmed_path: str = None, scopus_path: str = Non
     ## PULL DOIs AND TITLES ##
     ##########################
 
-    def doi_pull(summary_table, table_name):
+    def doi_pull(summary_table: pd.DataFrame, table_name: str):
         '''
         This function pulls DOIs for papers from the LitPortal table 
 
-        summary_table: (pandas DataFrame) The table exported from LitPortal
-        table_name: (str) The desired name of the table in the column
+        Parameters
+        ----------
+        summary_table
+            The table exported from LitPortal
+        
+        table_name
+            The desired name of the table in the column
+
+        Returns
+        -------
+            A pandas DataFrame with DOIs 
         '''
         if summary_table is None:
             return None
@@ -90,25 +179,22 @@ def litportal_deduplicate_papers(pubmed_path: str = None, scopus_path: str = Non
                     "DOI": summary_table["DOI"][summary_table["DOI"].isna() == False]
                 })
             )
-    
-    def text_clean(string):
-        '''
-        This function cleans up the text of the input string, removing nonalphanumerics.
-        This is essential to proper collapsing papers based on title. 
 
-        string: (str) The input string to clean
+    def title_pull(summary_table: pd.DataFrame, table_name: str):
+        '''
+        This function pulls titles for papers from the LitPortal table for whenever a DOI is not available. 
+
+        Parameters
+        ----------
+        summary_table
+            The table exported from LitPortal
         
-        '''
-        return(''.join(char for char in string.lower() if char.isalnum() or char == " "))
-    
+        table_name
+            The desired name of the table in the column
 
-    def title_pull(summary_table, table_name):
-        '''
-        This function pulls titles for papers from the LitPortal table for whenever
-        a DOI is not available. 
-
-        summary_table: (pandas DataFrame) The table exported from LitPortal
-        table_name: (str) The desired name of the table in the column
+        Returns
+        -------
+            A pandas DaataFrame with paper titles
         '''
         if summary_table is None:
             return None
@@ -116,41 +202,15 @@ def litportal_deduplicate_papers(pubmed_path: str = None, scopus_path: str = Non
             return(
                 pd.DataFrame({
                     table_name: [str(x) for x in summary_table["OriginId"][summary_table["DOI"].isna()]],
-                    "Title": [text_clean(x) for x in summary_table["Title"][summary_table["DOI"].isna()]]
+                    "Title": [_text_clean(x) for x in summary_table["Title"][summary_table["DOI"].isna()]]
                 })
             )
-    
-    def table_merge(pubmed_info, scopus_info, osti_info):
-        '''
-        Merge DOI or Title tables from doi_pull and title_pull. This function allows
-        for proper merging, for any combination of PubMed, Scopus, or OSTI LitPortal tables.
-
-        pubmed_info: (DataFrame) The output from doi_pull or title_pull from pubmed
-        scopus_info: (DataFrame) The output from doi_pull or title_pull from scopus
-        osti_info: (DataFrame) The output from doi_pull and title_pull from osti        
-        '''
-
-        # Merge as long as the dataframe is not none
-        summary_tables = [pubmed_info, scopus_info, osti_info]
-        exists = [pubmed_info is not None, scopus_info is not None, osti_info is not None]
-        position = [pos for pos in range(len(exists)) if exists[pos] == True]
-
-        # If length 1, no merging is required. If length 2, merge the two together. If length
-        # 3, merge all three togeter. 
-        if sum(exists) == 1: 
-            merged = summary_tables[position[0]]
-        elif sum(exists) == 2:
-            merged = pd.merge(summary_tables[position[0]], summary_tables[position[1]], how = "outer")
-        else: 
-            merged = pubmed_info.merge(scopus_info, how = "outer").merge(osti_info, how = "outer")
-
-        return(merged)
 
     # Create the table of papers 
     paper_table = pd.concat([
-        table_merge(doi_pull(pubmed, "pubmed"), doi_pull(scopus, "scopus"), doi_pull(osti, "osti")),
-        table_merge(title_pull(pubmed, "pubmed"), title_pull(scopus, "scopus"), title_pull(osti, "osti"))
-    ]).reset_index(drop = True)
+        _table_merge(doi_pull(pubmed, "pubmed"), doi_pull(scopus, "scopus"), doi_pull(osti, "osti")),
+        _table_merge(title_pull(pubmed, "pubmed"), title_pull(scopus, "scopus"), title_pull(osti, "osti"))
+    ]).reset_index(drop = True)[["Title", "DOI", "pubmed", "scopus", "osti"]]
 
     # Return the paper table
     return(paper_table)
