15f75c873ac740b35f6a373323939a3925b654b6
David-Degnan <ddegnan@uoregon.edu>
2025-07-09 16:44:33 -0700

    Added gene pulling from GTF file

--- src/DancePartner/pull_ome.py
+++ src/DancePartner/pull_ome.py
@@ -22,7 +22,7 @@ def pull_proteome(proteome_id: str, output_directory: str):
     
     Returns
     -------
-        Protein IDs and their synonyms in a text file
+    Protein IDs and their synonyms in a text file
     """
 
     # Construct the URL
@@ -89,3 +89,67 @@ def pull_proteome(proteome_id: str, output_directory: str):
             return(proteome)
     except:
         print(proteome_id + " is not recognized as a proper proteome_id")
+
+def pull_genome_from_GTF(GTF_file: str, output_directory: str):
+    '''
+    Extract the Gene IDs and Gene Names from a GTF file
+
+    Parameters
+    ----------
+    GFT_file
+        Path to the GTF genome file downloaded from ENSEMBL: https://www.ensembl.org/index.html
+    
+    output_directory
+        Path specifying where to write the result within the current directory. 
+    
+    Returns
+    -------
+    Gene IDs and their synonyms in a text file
+    '''
+
+    # Hold all the gene names
+    gene_id = []
+    gene_names = []
+
+    # Extract all the gene names 
+
+    # Open the file
+    with open(GTF_file, "r") as file:
+        
+        # Iterate through the file, searching for all gene names
+        for line in file:
+
+            # If the item is a gene name, extract the value
+            if "gene_id" in line and "gene_name" in line:
+
+                # Add identifier
+                gene_id.append(line.split("gene_id")[1].strip().split('"')[1])
+
+                # Find all genes
+                genes = line.split("gene_name")[1].strip().split('"')[1].split("{}")
+
+                # Remove numerics
+                gene_list = []
+                for gene in genes:
+                    try:
+                        float(gene)
+                    except:
+                        gene_list.append(gene.split(":")[-1])
+
+                # Add all genes that are not numberics       
+                gene_names.append("; ".join(gene_list))
+
+    # Start dataframe
+    genome = pd.DataFrame({
+        "Gene": gene_id,
+        "Synonyms": gene_names
+    }).drop_duplicates().reset_index(drop = True)
+
+    # Drop any cases where there is not a synonym or ID
+    genome = genome[(genome["Gene"] != "") & (genome["Synonyms"] != "")].reset_index(drop = True)
+
+    # Write genome
+    if output_directory is not None:
+        genome.to_csv(os.path.join(output_directory, "genome.txt"), index=False, sep = "\t")
+    else:
+        return(genome)
